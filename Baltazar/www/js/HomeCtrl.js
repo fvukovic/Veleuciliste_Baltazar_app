4bf854ce1cd7adbbd5987fae60783a4d798f1b8d
fvukovic <fvukovic@foi.hr>
2017-06-02 14:03:05 +0200

    ispravljen redoslijed vijesti i description vijesti

--- Baltazar/www/js/HomeCtrl.js
+++ Baltazar/www/js/HomeCtrl.js
@@ -1,15 +1,23 @@
 angular.module('starter')
   .controller('HomeCtrl', function ($scope, $stateParams, $http) { 
- $scope.vijesti;
+      $scope.vijesti;
+       $scope.markers = [];
+
+    
       var request = $http({
                     method: "GET",
                     url: 'https://baltazarapp.bak.hr/rest/newsList.php',
                     headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
                 });
                 request.success(function (data) {
+                  for(var v=0;v<data.length;v++){
+                      
+                  }
                   $scope.vijesti = data;
                   console.log(data);
                 });
+
+      
 
 
  
