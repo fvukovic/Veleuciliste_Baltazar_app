a5803e54fda9fbaa5e73f0dd657cc8498152289b
fvukovic <fvukovic@foi.hr>
2017-06-02 11:36:48 +0200

    Ureden profil
    Ureden profil

--- Baltazar/www/js/EditProfilCtrl.js
+++ Baltazar/www/js/EditProfilCtrl.js
@@ -1,17 +1,17 @@
 angular.module('starter')
-    .controller('EditProfilCtrl', function ($scope, $stateParams, $http,$state) {
+    .controller('EditProfilCtrl', function ($scope, $stateParams, $http, $state) {
         $scope.smjerovi
-          var request = $http({
-                    method: "GET",
-                    url: 'https://baltazarapp.bak.hr/rest/smjerovi.php',
-                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-                });
-                request.success(function (data) {
-                  $scope.smjerovi = data;
-                  console.log(data);
-                });
+        var request = $http({
+            method: "GET",
+            url: 'https://baltazarapp.bak.hr/rest/smjerovi.php',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        });
+        request.success(function (data) {
+            $scope.smjerovi = data;
+            console.log(data);
+        });
 
-$scope.smjerovi;
+        $scope.smjerovi;
         $scope.godine;
         $scope.odabraniSmjer;
         $scope.odabraniSmjerg;
@@ -43,10 +43,33 @@ $scope.smjerovi;
         }
 
 
-
-        $scope.saveData = function () { 
+        $scope.saveData = function () {
+            if($scope.odabraniSmjer=="" || $scope.odabranaGodina=="" || document.getElementById("ime").value=="" || document.getElementById("prezime").value=="" || document.getElementById("adresa").value=="" || document.getElementById("tel").value=="" || document.getElementById("mail").value==""){
+                alert("Nisu svi podaci upisani");
+                return 0;
+            }
             window.localStorage.setItem("smjer", $scope.odabraniSmjer);
-            window.localStorage.setItem("godina",  $scope.odabranaGodina); 
+            window.localStorage.setItem("godina", $scope.odabranaGodina);
+            window.localStorage.setItem("adresa", document.getElementById("adresa").value);
+            window.localStorage.setItem("tel", document.getElementById("tel").value);
+            window.localStorage.setItem("ime", document.getElementById("ime").value);
+            window.localStorage.setItem("prezime", document.getElementById("prezime").value);
+            window.localStorage.setItem("mail", document.getElementById("mail").value);
+            var request = $http({
+                method: "POST",
+                url: 'https://baltazarapp.bak.hr/rest/newsDetails.php',
+                data: {
+                    push: "1", os: "android", token: window.localStorage.getItem("token"), godina: $scope.odabranaGodina, smjer: $scope.odabraniSmjer, ime: document.getElementById("ime").value, prezime: document.getElementById("prezime")
+                        .value, telefon: document.getElementById("tel").value, adresa: document.getElementById("adresa").value, email: document.getElementById("mail").value
+                },
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+
+            });
+            request.success(function (data) {
+
+                console.log("Prijava" + data);
+                
+            });
             $state.go("bapp.profil");
 
         }
--- Baltazar/www/js/ProfilCtrl.js
+++ Baltazar/www/js/ProfilCtrl.js
@@ -9,7 +9,7 @@ angular.module('starter')
       for (var v = 0; v < data.length; v++) {
         
         if (data[v]["id"] == window.localStorage.getItem("smjer")) {
-          $scope.smjer = data[0]["title"] ; 
+          $scope.smjer = data[v]["title"] ; 
         }
       }
     });
@@ -22,7 +22,7 @@ angular.module('starter')
       for (var v = 0; v < data.length; v++) {
         
         if (data[v]["id"] == window.localStorage.getItem("godina")) {
-          $scope.godina = data[0]["title"] ;alert();
+          $scope.godina = data[0]["title"] ; 
         }
       }
     });
@@ -30,6 +30,36 @@ angular.module('starter')
      if (0 == window.localStorage.getItem("godina")) {
           $scope.godina ="Neregistrirani korisnik";
         }
+     if (null == window.localStorage.getItem("ime") || "" == window.localStorage.getItem("ime")) {
+          $scope.ime ="IME";
+        } else{
+
+          $scope.ime=window.localStorage.getItem("ime");
+        }  
+        if (null == window.localStorage.getItem("prezime") || "" == window.localStorage.getItem("prezime")) {
+          $scope.prezime ="PREZIME";
+        } else{
+
+          $scope.prezime=window.localStorage.getItem("prezime");
+        }  
+        if (null == window.localStorage.getItem("adresa") || "" == window.localStorage.getItem("adresa")) {
+          $scope.adresa ="Adresa nije unesena";
+        } else{
+
+          $scope.adresa=window.localStorage.getItem("adresa");
+        }  
+         if (null == window.localStorage.getItem("mail") || "" == window.localStorage.getItem("mail")) {
+          $scope.mail ="E-mail nije unesen";
+        } else{
+
+          $scope.mail=window.localStorage.getItem("mail");
+        }  
+         if (null == window.localStorage.getItem("tel") || "" == window.localStorage.getItem("tel")) {
+          $scope.tel ="Telefon nije unesen";
+        } else{
+
+          $scope.tel=window.localStorage.getItem("tel");
+        } 
     console.log("aaaaaaaaaa");
   });
 
